15a6aab3f2f92ec5c1bed2cddcc4ff145f4b6ad7
taufikaji46 <taufikaji46@gmail.com>
2021-10-09 20:39:58 +0700

    pembaharuan program servocontrol

--- control_stmduino_multi/control_stmduino_multi.ino
+++ control_stmduino_multi/control_stmduino_multi.ino
@@ -1,8 +1,3 @@
-//Servo ASME 04A 260kg Pulse Width 500-2500 dengan degree 0-270
-
-// posisi ngambil = 2150
-// posisi lepas panah 750
-
 /*
  * servo untuk robot jari 1 dof itu range 1700-2500max 1700buka 2500 tutup
  * CCW untuk naik
@@ -22,18 +17,17 @@ Servo gripperServo3;
 Servo gripperServo4;
 Servo gripperServo5;
 
-int currentPos=1600,servoTargetPos=1600;
-int currentPos2=2200,servoTargetPos2=2200;
-int currentPos3=2200,servoTargetPos3=2200;
-int currentPos4=1900,servoTargetPos4=1900;
-int currentPos5=1800,servoTargetPos5=1800;
+int currentPos=2400,servoTargetPos=2400;
+int currentPos2=2400,servoTargetPos2=2400;
+int currentPos3=2400,servoTargetPos3=2400;
+int currentPos4=2400,servoTargetPos4=2400;
+int currentPos5=2400,servoTargetPos5=2400;
 
 unsigned long lastTime;
 uint32_t sample_time;
 #define servoTimeInterval 15000
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 //  Serial.setTimeout(1000000);
   Serial.setTimeout(1);
@@ -43,13 +37,13 @@ void setup() {
   gripperServo4.attach(PA3);
   gripperServo5.attach(PA5);
 
+  //setting awal posisi servo
   gripperServo.writeMicroseconds(currentPos);
   gripperServo2.writeMicroseconds(currentPos2);
   gripperServo3.writeMicroseconds(currentPos3);
   gripperServo4.writeMicroseconds(currentPos4);
   gripperServo5.writeMicroseconds(currentPos5);
 
-  //Serial.println("Posisi awal di 1250");
 }
 //coba pulse
 String rea;
@@ -57,25 +51,24 @@ int coba=1700;
 int coba2=1700;
 int coba3=1700;
 int coba4=1700;
-int coba5=1700;
+int coba_last=1700;
 void loop() {
-//  // put your main code here, to run repeatedly:
-////  servoTargetPos = 670;
-// 
-////
   if (Serial.available()>1) {
-//    coba = Serial.parseInt();
-//    coba2 = Serial.parseInt();
-//    coba3 = Serial.parseInt();
-//    coba4 = Serial.parseInt();
-//    coba5 = Serial.parseInt();
-  rea = Serial.readString();
+    coba = Serial.parseInt();
+    coba2 = Serial.parseInt();
+    coba3 = Serial.parseInt();
+    coba4 = Serial.parseInt();
+    coba_last = Serial.parseInt();
+//  rea = Serial.readString();
   }
-  terimaDanKasih(rea);
+// untuk menerima data dari program python dari laptop  
+//  terimaDanKasih(rea);
+  
+// if coba ini untuk aktifkan 
+  //servo melalui serial monitor
   if(coba!=0){
     servoTargetPos=coba;
   }
-//////  }
   if(coba2!=0){
     servoTargetPos2=coba2;
   }
@@ -85,35 +78,16 @@ void loop() {
   if(coba4!=0){
     servoTargetPos4=coba4;
   }
-  if(coba5!=0){
-    servoTargetPos5=coba5;
+  if(coba_last!=0){
+    servoTargetPos5=coba_last;
   }
 
-//if (currentPos <= 1600) servoTargetPos = 2500;
-//else if (currentPos >=2500) servoTargetPos = 1600;
-//if (currentPos2 <= 1700) servoTargetPos2 = 2500;
-//else if (currentPos2 >=2500) servoTargetPos2 = 1700;
-//if (currentPos3 <= 1700) servoTargetPos3 = 2500;
-//else if (currentPos3 >=2500) servoTargetPos3 = 1700;
-//if (currentPos4 <= 1700) servoTargetPos4 = 2500;
-//else if (currentPos4 >=2500) servoTargetPos4 = 1700;
-//if (currentPos5 <= 1700) servoTargetPos5 = 2500;
-//else if (currentPos5 >=2500) servoTargetPos5 = 1700;
+//fungsi untuk menjalankan servo
 gripperServoControl();
 
-//  b = map(currentPos, 0, 270, 500, 2500);
-//  gripperServo.writeMicroseconds(b);
-  
+//aktifkan bila perlu check data 
+//checkingParsing();
 
-  
- 
-//  sudut=gripperServo.readMicroseconds();
-//  Serial.print(currentPos);
-//  Serial.print(" ");
-//  Serial.print(sudut);
-//  Serial.print(" ");
-//  Serial.println(b);
-//  
 }
 
 
@@ -128,31 +102,47 @@ void gripperServoControl()
       
       if((micros() - lastTime) > servoTimeInterval)
       {
-        if(abs(currentPos - servoTargetPos) >= 20) currentPos += 24;//perubahan pada posisi tertentu
-        else if(abs(currentPos - servoTargetPos) > 4 && abs(currentPos - servoTargetPos) < 20 ) currentPos += 3;
-        else {
-          currentPos++;
+        if(currentPos < servoTargetPos){
+          if(abs(currentPos - servoTargetPos) >= 20) currentPos += 24;//perubahan pada posisi tertentu
+          else if(abs(currentPos - servoTargetPos) > 4 && abs(currentPos - servoTargetPos) < 20 ) currentPos += 3;
+          else {
+              currentPos++;
+          }
         }
-        if(abs(currentPos2 - servoTargetPos2) >= 20) currentPos2+= 24;//perubahan pada posisi tertentu
-        else if(abs(currentPos2 - servoTargetPos2) > 4 && abs(currentPos2 - servoTargetPos2) < 20 ) currentPos2 += 3;
-        else {
-          currentPos2++;
+    
+        if(currentPos2 < servoTargetPos2){
+          if(abs(currentPos2 - servoTargetPos2) >= 20) currentPos2+= 24;//perubahan pada posisi tertentu
+          else if(abs(currentPos2 - servoTargetPos2) > 4 && abs(currentPos2 - servoTargetPos2) < 20 ) currentPos2 += 3;
+          else {
+              currentPos2++;
+          }
         }
-        if(abs(currentPos3 - servoTargetPos3) >= 20) currentPos3 += 24;//perubahan pada posisi tertentu
-        else if(abs(currentPos3 - servoTargetPos3) > 4 && abs(currentPos3 - servoTargetPos3) < 20 ) currentPos3 += 3;
-        else {
-          currentPos3++;
+                
+        if(currentPos3 < servoTargetPos3){
+          if(abs(currentPos3 - servoTargetPos3) >= 20) currentPos3 += 24;//perubahan pada posisi tertentu
+          else if(abs(currentPos3 - servoTargetPos3) > 4 && abs(currentPos3 - servoTargetPos3) < 20 ) currentPos3 += 3;
+          else {
+              currentPos3++;
+          }
         }
-        if(abs(currentPos4 - servoTargetPos4) >= 20) currentPos4 += 24;//perubahan pada posisi tertentu
-        else if(abs(currentPos4 - servoTargetPos4) > 4 && abs(currentPos4 - servoTargetPos4) < 20 ) currentPos4 += 3;
-        else {
-          currentPos4++;
+           
+        if(currentPos4 < servoTargetPos4){
+          if(abs(currentPos4 - servoTargetPos4) >= 20) currentPos4 += 24;//perubahan pada posisi tertentu
+          else if(abs(currentPos4 - servoTargetPos4) > 4 && abs(currentPos4 - servoTargetPos4) < 20 ) currentPos4 += 3;
+          else {
+              currentPos4++;
+          }
         }
-        if(abs(currentPos5 - servoTargetPos5) >= 20) currentPos5 += 24;//perubahan pada posisi tertentu
-        else if(abs(currentPos5 - servoTargetPos5) > 4 && abs(currentPos5 - servoTargetPos5) < 20 ) currentPos5 += 3;
-        else {
-          currentPos5++;
+        
+        if(currentPos5 < servoTargetPos5){
+          if(abs(currentPos5 - servoTargetPos5) >= 20) currentPos5 += 24;//perubahan pada posisi tertentu
+          else if(abs(currentPos5 - servoTargetPos5) > 4 && abs(currentPos5 - servoTargetPos5) < 20 ) currentPos5 += 3;
+          else {
+              currentPos5++;
+          }
         }
+        
+        
 //
       lastTime = micros();
       }
@@ -162,32 +152,45 @@ void gripperServoControl()
     {
       if((micros()-lastTime) > servoTimeInterval)
       {
-        if(abs(currentPos - servoTargetPos) >= 20) currentPos -= 24;
-        else if(abs(currentPos - servoTargetPos) > 4 && abs(currentPos - servoTargetPos) < 20 ) currentPos -= 3;
-        else {
-          currentPos--;  
-        }
-        if(abs(currentPos2 - servoTargetPos2) >= 20) currentPos2 -= 24;
-        else if(abs(currentPos2 - servoTargetPos2) > 4 && abs(currentPos2 - servoTargetPos2) < 20 ) currentPos2 -= 3;
-        else {
-          currentPos2--;  
-        }
-        if(abs(currentPos3 - servoTargetPos3) >= 20) currentPos3 -= 24;
-        else if(abs(currentPos3 - servoTargetPos3) > 4 && abs(currentPos3 - servoTargetPos3) < 20 ) currentPos3 -= 3;
-        else {
-          currentPos3--;  
-        }
-        if(abs(currentPos4 - servoTargetPos4) >= 20) currentPos4 -= 24;
-        else if(abs(currentPos4 - servoTargetPos4) > 4 && abs(currentPos4 - servoTargetPos4) < 20 ) currentPos4 -= 3;
-        else {
-          currentPos4--;  
-        }
-        if(abs(currentPos5 - servoTargetPos5) >= 20) currentPos5 -= 24;
-        else if(abs(currentPos5 - servoTargetPos5) > 4 && abs(currentPos5 - servoTargetPos5) < 20 ) currentPos5 -= 3;
-        else {
-          currentPos5--;  
+        if(currentPos > servoTargetPos){
+          if(abs(currentPos - servoTargetPos) >= 20) currentPos -= 24;
+          else if(abs(currentPos - servoTargetPos) > 4 && abs(currentPos - servoTargetPos) < 20 ) currentPos -= 3;
+          else {
+              currentPos--;  
+          }
         }
         
+        if(currentPos2 > servoTargetPos2){
+          if(abs(currentPos2 - servoTargetPos2) >= 20) currentPos2 -= 24;
+          else if(abs(currentPos2 - servoTargetPos2) > 4 && abs(currentPos2 - servoTargetPos2) < 20 ) currentPos2 -= 3;
+          else {
+              currentPos2--;  
+          }
+        }
+        
+        if(currentPos3 > servoTargetPos3){
+          if(abs(currentPos3 - servoTargetPos3) >= 20) currentPos3 -= 24;
+          else if(abs(currentPos3 - servoTargetPos3) > 4 && abs(currentPos3 - servoTargetPos3) < 20 ) currentPos3 -= 3;
+          else {
+              currentPos3--;  
+          }
+        }
+        
+        if(currentPos4 > servoTargetPos4){
+          if(abs(currentPos4 - servoTargetPos4) >= 20) currentPos4 -= 24;
+          else if(abs(currentPos4 - servoTargetPos4) > 4 && abs(currentPos4 - servoTargetPos4) < 20 ) currentPos4 -= 3;
+          else {
+              currentPos4--;  
+          }
+        }
+       
+        if(currentPos5 > servoTargetPos5){
+          if(abs(currentPos5 - servoTargetPos5) >= 20) currentPos5 -= 24;
+          else if(abs(currentPos5 - servoTargetPos5) > 4 && abs(currentPos5 - servoTargetPos5) < 20 ) currentPos5 -= 3;
+          else {
+              currentPos5--;  
+          }
+        }
         lastTime = micros();
       }
     }
@@ -216,9 +219,25 @@ void terimaDanKasih (String dataSer){
       coba4 = dataSer.substring(a,a+4).toInt();
       break;
       case 4:
-      coba5 = dataSer.substring(a,a+4).toInt();
+      coba_last = dataSer.substring(a,a+4).toInt();
       break;
     }
     a += 4;
   }
   }
+
+void checkingParsing(){
+  Serial.print(currentPos);
+  Serial.print(" nilai coba5 ");
+  Serial.print(coba);
+  Serial.print(" ");
+  Serial.print(servoTargetPos);
+  Serial.print(" ");
+  Serial.print(servoTargetPos2);
+  Serial.print(" ");
+  Serial.print(servoTargetPos3);
+  Serial.print(" ");
+  Serial.print(servoTargetPos4);
+  Serial.print(" ");
+  Serial.println(servoTargetPos5);
+}
